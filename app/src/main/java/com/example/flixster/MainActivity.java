5109876f4edad89cf7b2fb872c0c53260da1c7b2
Roberto Berwa <rberwa@fb.com>
2020-06-24 11:20:43 -0700

    add a movie request and parse feature

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.codepath.asynchttpclient.callback.TextHttpResponseHandler;
+import com.example.flixster.models.Movie;
+import com.facebook.stetho.inspector.network.ResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //requesting movies using the AsyncHttpClient to make requests to the movies API
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    movies = Movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies" + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "hit json exception", e);
+
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String errorResponse, Throwable t) {
+                    // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d(TAG, "onFailure");
+
+            }
+        }
+        );
     }
 }
